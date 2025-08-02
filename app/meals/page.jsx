8f11900309a40f5943da44dcfe61aaee0020193b
Borna Mashayekh <bornamashaye@gmail.com>
2025-08-02 22:02:23 +0330

    updating page.jsx  for mapping meals and building setup for using database etc

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,7 +1,24 @@
+import MealsGrid from "@/comonents/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
 export default function MealsPage() {
-    return (
-        <div>
-            <h>Meals Page</h>
-        </div>
-    );
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals ,Created{" "}
+          <span className={classes.highlight}>By you</span>
+        </h1>
+        <p>
+          Choose Your Favorite Recipe And Cook It Yourself , It Is Easy And Fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+          <main className={classes.main}>
+              <MealsGrid meals={[]}></MealsGrid>
+      </main>
+    </>
+  );
 }
