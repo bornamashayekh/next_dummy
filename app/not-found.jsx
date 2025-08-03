bb9b6345121cf3b2ca8ac6fe69205e3a72786688
Borna Mashayekh <bornamashaye@gmail.com>
2025-08-03 12:43:01 +0330

    adding customize not found page for the project

---
+++ app/not-found.jsx
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>Not Found</h1>
+        <p>Unfortunatly ,  we could not found this page</p>
+    </main>
+}
