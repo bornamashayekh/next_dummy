f77a30a11f27d312d39a6c0b07db601849d102fc
Borna Mashayekh <bornamashaye@gmail.com>
2025-08-03 13:01:19 +0330

    Displays detailed meal information
    Implements a dynamic meal details page to show complete information about a selected meal.
    
    This includes the meal's image, title, creator, summary, and instructions.
    Also includes a not-found page in case the meal is not found.
    
    CSS modules were added to style the meal details page.

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,7 +1,30 @@
-export default function MealPage() {
-    return (
-        <div>
-            <h>Meal Page</h>
-        </div>
-    );
+import { GetMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+export default function MealPage({params}) {
+  const meal = GetMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, '</br>');
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} />
+              </div>
+              <div className={classes.headerText}>
+                  <h1>{meal.title}</h1>
+                  <p className={classes.creator}> by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a></p>
+                  <p className={classes.summary}>{meal.summary}</p>
+              </div>
+      </header>
+          <main>
+              <p className={classes.instructions} dangerouslySetInnerHTML={{__html:meal.instructions}}>
+                  
+              </p>
+      </main>
+    </>
+  );
 }
---
+++ app/meals/not-found.jsx
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>MEAL Not Found</h1>
+        <p>Unfortunatly ,  we could not found the requested page or the meal data</p>
+    </main>
+}
